80ca844f2278c2f5597f3c2df064f0e193ac99fb
Kevin Lacabane <klacabane@gmail.com>
2015-05-17 19:22:55 +0200

    eh

--- main.go
+++ main.go
@@ -2,117 +2,40 @@ package main
 
 import (
 	"container/ring"
+	"io/ioutil"
+	"os/exec"
+	"path/filepath"
 	"strings"
 
 	ui "github.com/gizak/termui"
 )
 
-// Overall description
-// View
-//  event listener
-//  Circular linked list of Block
-//  Block is an interface
-// ViewModel
+const DATA_DIR = "data"
 
-type View struct {
-	eventCh <-chan ui.Event
-	block   *ring.Ring
-}
+var player *AudioPlayer
 
-func NewView(blocks ...*List) *View {
-	v := new(View)
-	v.block = ring.New(len(blocks))
-	for _, b := range blocks {
-		v.block.Value = b
-		v.block = v.block.Next()
+func walk() []*Playlist {
+	var pl []*Playlist
+
+	dirs, err := ioutil.ReadDir(DATA_DIR)
+	if err != nil {
+		panic(err)
 	}
-	return v
-}
-
-func (v *View) Current() *List {
-	return v.block.Value.(*List)
-}
-
-func (v *View) NextBlock() {
-	v.Current().Border.FgColor = ui.ColorWhite
-	v.block = v.block.Next()
-	v.Current().Border.FgColor = ui.ColorRed
-}
-
-func (v *View) Render() {
-	for {
-		select {
-		case e := <-v.eventCh:
-			if e.Type == ui.EventKey && e.Key == ui.KeyArrowUp {
-				v.Current().selectPrevItem()
-			}
-			if e.Type == ui.EventKey && e.Key == ui.KeyArrowDown {
-				v.Current().selectNextItem()
-			}
-			if e.Type == ui.EventKey && e.Key == ui.KeyTab {
-				v.NextBlock()
-			}
-			if e.Type == ui.EventKey && e.Key == ui.KeyEnter {
-				switch t := ViewModel.target.(type) {
-				case *Playlist:
-					// display the tracks of the selected playlist
-					// in the tracks block
-					v.NextBlock()
-					v.Current().current = 0
-
-					names := make([]string, len(t.Tracks))
-					for i := 0; i < len(t.Tracks); i++ {
-						var pre string
-						if i == v.Current().current {
-							pre = "[-] "
-						} else {
-							pre = "[ ] "
-						}
-						names[i] = pre + t.Tracks[i].Name
-					}
-					v.Current().Items = names
-				case *Track:
-					// play this like ther is no tomorrow
-				}
-			}
-
-			if e.Type == ui.EventKey && e.Key == ui.KeyEsc {
-				return
-			}
-		default:
-			blocks := make([]ui.Bufferer, 0)
-			v.block.Do(func(x interface{}) {
-				blocks = append(blocks, x.(ui.Bufferer))
-			})
-			ui.Render(blocks...)
+	for _, dir := range dirs {
+		p := &Playlist{Name: dir.Name()}
+		files, err := ioutil.ReadDir(filepath.Join(DATA_DIR, p.Name))
+		if err != nil {
+			panic(err)
 		}
+		for _, f := range files {
+			p.Tracks = append(p.Tracks, &Track{
+				Name: strings.TrimSuffix(f.Name(), filepath.Ext(f.Name())),
+				Path: filepath.Join(DATA_DIR, p.Name, f.Name()),
+			})
+		}
+		pl = append(pl, p)
 	}
-}
-
-var ViewModel struct {
-	playlists []*Playlist
-	target    interface{}
-}
-
-type Playlist struct {
-	Name   string
-	Tracks []*Track
-}
-
-var tracks = []string{"track one", "track two", "track three", "track four"}
-var cpt = 0
-
-func NewPlaylist(name string) *Playlist {
-	p := &Playlist{
-		Name:   name,
-		Tracks: []*Track{&Track{tracks[cpt]}, &Track{tracks[cpt+1]}},
-	}
-	cpt = cpt + 2
-	return p
-}
-
-type Track struct {
-	Name string
+	return pl
 }
 
 func main() {
@@ -120,58 +43,292 @@ func main() {
 		panic(err)
 	}
 
-	ViewModel.playlists = []*Playlist{NewPlaylist("foo"), NewPlaylist("bar")}
-	ViewModel.target = ViewModel.playlists[0]
+	playlists := walk()
+	pl := NewPlaylistCmp(playlists)
 
-	view := NewView(
-		NewList([]string{"foo", "bar"}, 0),
-		NewList([]string{"place", "holder"}, 15))
+	view := NewView(pl)
 	view.eventCh = ui.EventCh()
 
 	view.Render()
 }
 
-type List struct {
+type AudioPlayer struct {
+	tracks []*Track
+	track  *Track
+	repeat bool
+}
+
+func (p *AudioPlayer) Set(tracks []*Track, start int) {
+	p.tracks = tracks
+}
+
+func (p *AudioPlayer) Play() error {
+	cmd := exec.Command("afplay", p.track.Path)
+	return cmd.Start()
+}
+
+func (p *AudioPlayer) Pause() error {
+	// kill -17 pid @pause
+	// kill -19 pid @resume
+	return nil
+}
+
+func (p *AudioPlayer) Stop() error {
+	return nil
+}
+
+func (p *AudioPlayer) Prev() error {
+	return nil
+}
+
+func (p *AudioPlayer) Next() error {
+	return nil
+}
+
+type View struct {
+	eventCh   <-chan ui.Event
+	component *ring.Ring
+}
+
+func NewView(cmp ...Component) *View {
+	v := new(View)
+	for i, c := range cmp {
+		if i == 0 {
+			c.Focus(true)
+			v.component = cmpRing(c)
+		} else {
+			v.component = v.component.Move(v.component.Len() - 1)
+			v.component = v.component.Link(cmpRing(c))
+		}
+	}
+	return v
+}
+
+func cmpRing(cmp Component) *ring.Ring {
+	if cmp == nil {
+		return nil
+	}
+	r := new(ring.Ring) //ring.New(1)
+	r.Value = cmp
+	if cmp.Child() != nil {
+		r.Link(cmpRing(cmp.Child()))
+	}
+	return r
+}
+
+func (v *View) Current() Component {
+	return v.component.Value.(Component)
+}
+
+func (v *View) NextComponent() {
+	v.component.Value.(Component).Focus(false)
+	v.component = v.component.Next()
+	v.component.Value.(Component).Focus(true)
+}
+
+func (v *View) Render() {
+	for {
+		select {
+		case e := <-v.eventCh:
+			if e.Type == ui.EventKey && e.Key == ui.KeyEsc {
+				return
+			}
+			if e.Type == ui.EventKey && e.Key == ui.KeyTab {
+				v.NextComponent()
+			} else {
+				v.Current().Handle(e)
+			}
+		default:
+			cmps := make([]ui.Bufferer, 0)
+			v.component.Do(func(x interface{}) {
+				cmps = append(cmps, x.(ui.Bufferer))
+			})
+			ui.Render(cmps...)
+		}
+	}
+}
+
+type Playlist struct {
+	Name   string
+	Tracks []*Track // doubly linked list?
+}
+
+type Track struct {
+	Name string
+	Path string
+}
+
+type Component interface {
+	Handle(ui.Event)
+	Child() Component
+	Focus(bool)
+}
+
+type PlaylistCmp struct {
 	*ui.List
+	playlists []*Playlist
+	child     Component
+	current   int
+}
+
+func NewPlaylistCmp(playlists []*Playlist) *PlaylistCmp {
+	c := &PlaylistCmp{
+		ui.NewList(),
+		playlists,
+		NewTrackCmp(playlists[0].Tracks),
+		0,
+	}
+	c.Focus(false)
+	c.Width = 15
+	c.Height = 10
+	for i, pl := range playlists {
+		var pre string
+		if i == 0 {
+			pre = "[-] "
+		} else {
+			pre = "[ ] "
+		}
+		c.Items = append(c.Items, pre+pl.Name)
+	}
+	return c
+}
+
+func (c *PlaylistCmp) Child() Component {
+	return c.child
+}
+func (c *PlaylistCmp) Focus(focus bool) {
+	if focus {
+		c.Border.FgColor = ui.ColorWhite
+	} else {
+		c.Border.FgColor = ui.ColorBlack
+	}
+}
+
+func (c *PlaylistCmp) Handle(e ui.Event) {
+	if e.Type == ui.EventKey && e.Key == ui.KeyArrowUp {
+		c.Items[c.current] = strings.Replace(c.Items[c.current], "-", " ", 1)
+		if c.current > 0 {
+			c.current--
+		} else {
+			c.current = len(c.Items) - 1
+		}
+		c.Items[c.current] = strings.Replace(c.Items[c.current], " ", "-", 1)
+	}
+	if e.Type == ui.EventKey && e.Key == ui.KeyArrowDown {
+		c.Items[c.current] = strings.Replace(c.Items[c.current], "-", " ", 1)
+		if c.current+1 == len(c.Items) {
+			c.current = 0
+		} else {
+			c.current++
+		}
+		c.Items[c.current] = strings.Replace(c.Items[c.current], " ", "-", 1)
+	}
+	if e.Type == ui.EventKey && e.Key == ui.KeyEnter {
+		trackCmp := c.child.(*TrackCmp)
+		trackCmp.current = 0
+		pl := c.playlists[c.current]
+		trackCmp.tracks = pl.Tracks
+		names := make([]string, len(pl.Tracks))
+		for i, t := range pl.Tracks {
+			var pre string
+			if i == 0 {
+				pre = "[-] "
+			} else {
+				pre = "[ ] "
+			}
+			names[i] = pre + t.Name
+		}
+		trackCmp.Items = names
+	}
+
+}
+
+type TrackCmp struct {
+	*ui.List
+	tracks  []*Track
+	child   Component
 	current int
 }
 
-func NewList(items []string, x int) *List {
-	l := &List{
+func NewTrackCmp(tracks []*Track) *TrackCmp {
+	c := &TrackCmp{
 		ui.NewList(),
+		tracks,
+		nil,
 		0,
 	}
-	l.Width = 15
-	l.Height = 10
-	l.X = x
-
-	l.Items = append(l.Items, "[-] "+items[0])
-	for i := 1; i < len(items); i++ {
-		l.Items = append(l.Items, "[ ] "+items[i])
+	c.Focus(false)
+	c.Width = 25
+	c.Height = 10
+	c.X = 15
+	for i, tr := range tracks {
+		var pre string
+		if i == 0 {
+			pre = "[-] "
+		} else {
+			pre = "[ ] "
+		}
+		c.Items = append(c.Items, pre+tr.Name)
 	}
-	return l
+	return c
 }
 
-func (l *List) selectPrevItem() {
-	l.Items[l.current] = strings.Replace(l.Items[l.current], "-", " ", 1)
-	if l.current > 0 {
-		l.current--
-	} else {
-		l.current = len(l.Items) - 1
-	}
-	l.Items[l.current] = strings.Replace(l.Items[l.current], " ", "-", 1)
-
-	ViewModel.target = ViewModel.playlists[l.current]
+func (c *TrackCmp) Child() Component {
+	return c.child
 }
 
-func (l *List) selectNextItem() {
-	l.Items[l.current] = strings.Replace(l.Items[l.current], "-", " ", 1)
-	if l.current+1 == len(l.Items) {
-		l.current = 0
+func (c *TrackCmp) Focus(focus bool) {
+	if focus {
+		c.Border.FgColor = ui.ColorWhite
 	} else {
-		l.current++
+		c.Border.FgColor = ui.ColorBlack
 	}
-	l.Items[l.current] = strings.Replace(l.Items[l.current], " ", "-", 1)
+}
 
-	ViewModel.target = ViewModel.playlists[l.current]
+func (c *TrackCmp) Handle(e ui.Event) {
+	if e.Type == ui.EventKey && e.Key == ui.KeyArrowUp {
+		c.Items[c.current] = strings.Replace(c.Items[c.current], "-", " ", 1)
+		if c.current > 0 {
+			c.current--
+		} else {
+			c.current = len(c.Items) - 1
+		}
+		c.Items[c.current] = strings.Replace(c.Items[c.current], " ", "-", 1)
+	}
+	if e.Type == ui.EventKey && e.Key == ui.KeyArrowDown {
+		c.Items[c.current] = strings.Replace(c.Items[c.current], "-", " ", 1)
+		if c.current+1 == len(c.Items) {
+			c.current = 0
+		} else {
+			c.current++
+		}
+		c.Items[c.current] = strings.Replace(c.Items[c.current], " ", "-", 1)
+	}
+	if e.Type == ui.EventKey && e.Key == ui.KeyEnter {
+		_ = exec.Command("afplay", c.tracks[c.current].Path).Run()
+	}
+
+}
+
+type ControlsCmp struct {
+	controls []func() error
+	current  int
+}
+
+func NewControlsCmp() *ControlsCmp {
+	return &ControlsCmp{
+		[]func() error{
+			player.Play,
+			player.Pause,
+			player.Stop,
+		},
+		0,
+	}
+}
+
+func (c *ControlsCmp) Handle(e ui.Event) {
+	if e.Type == ui.EventKey && e.Key == ui.KeyEnter {
+		if err := c.controls[c.current](); err != nil {
+		}
+	}
 }
